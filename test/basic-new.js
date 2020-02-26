4992515c2acd9d0cf612b9c8c7d9c6bd27d092d4
Franz Heinzmann (Frando) <frando@unbiskant.org>
2020-02-26 12:08:08 +0100

    Add live read streams

--- test/basic-new.js
+++ test/basic-new.js
@@ -54,3 +54,39 @@ tape('replicate', t => {
     })
   })
 })
+
+tape.only('live', t => {
+  const feed1 = hypercore(ram)
+  const feed2 = hypercore(ram)
+  const feedRemote = hypercore(ram)
+  feedRemote.ready(() => {
+    const feed3 = hypercore(ram, feedRemote.key)
+    indexer.add(feed3)
+    replicate(feedRemote, feed3)
+  })
+  const indexer = new Indexer(mem())
+  indexer.add(feed1)
+  indexer.add(feed2)
+
+  feed1.append('1a')
+  feed2.append('2a')
+  feed1.append('1b')
+  feedRemote.append('3a')
+
+  const sub = indexer.createSubscription()
+  const rs = sub.createPullStream({ live: true })
+  const values = []
+  rs.on('data', msg => {
+    values.push(msg.value.toString())
+    if (values.length === 6) {
+      t.deepEqual(values.sort(), ['1a', '1b', '1c', '2a', '3a', '3b'])
+      rs.destroy()
+      t.end()
+    }
+  })
+
+  setTimeout(() => {
+    feed1.append('1c')
+    feedRemote.append('3b')
+  }, 10)
+})
